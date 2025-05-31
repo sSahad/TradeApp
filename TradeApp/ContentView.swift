6c3824f9fa46e0b6be5d35f809f97c9be9a93020
Syed Sadrul Ullah Sahad <syed.sahad@mindvalley.com>
2025-05-31 13:10:11 +0800

    Initial Commit

---
+++ TradeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradeApp
+//
+//  Created by Sahad on 31/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradeApp/TradeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeAppApp.swift
+//  TradeApp
+//
+//  Created by Sahad on 31/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TradeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
